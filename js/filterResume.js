707c9a215650618d99f6957b6fe73f013ee52401
krishna Kumar Singh <krishnakumarsingh@users.noreply.github.com>
2023-01-31 00:55:30 +0530

    Add

--- js/filterResume.js
+++ js/filterResume.js
@@ -58,12 +58,18 @@ Filter.prototype.resumeEventBind = function () {
 };
 
 Filter.prototype.loadFilterBtn = function () {
-    const prev = `<button id="prevResume" class="${this.currentData.length === 0 || (this.currentData && this.index === 0) ? 'hide' : ''}"><i class="fas fa-solid fa-caret-left"></i><span>Previous</span></button>`;
-    const nxt = `<button id="nextResume" class="${this.currentData.length === 0 || (this.currentData && this.currentData.length - 1 === this.index) ? 'hide' : ''}"><span>Next</span><i class="fas fa-solid fa-caret-right"></i></button>`;
-    const prevWrap = document.getElementById("prevWrap");
-    const nxtWrap = document.getElementById("nxtWrap");
-    prevWrap.innerHTML = prev;
-    nxtWrap.innerHTML = nxt;
+    const prevWrap = document.getElementById("prevResume");
+    const nxtWrap = document.getElementById("nextResume");
+    if(this.currentData.length === 0 || (this.currentData && this.index === 0)) {
+        prevWrap.style.display = "none";
+    } else {
+        prevWrap.style.display = "block";
+    }
+    if(this.currentData.length === 0 || (this.currentData && this.currentData.length - 1 === this.index)) {
+        nxtWrap.style.display = "none";
+    } else {
+        nxtWrap.style.display = "block";
+    }
 };
 
 Filter.prototype.loadFilterContainer = function () {
@@ -74,10 +80,6 @@ Filter.prototype.loadFilterContainer = function () {
         window.location.href = "./";
     });
     const filterContainer = document.getElementById("filterContainer");
-    let html = `<div id="prevWrap"></div>
-            <input type="text" name="search" id="searchResume" placeholder="Search for keywords to filter the result" />
-            <div id="nxtWrap"></div>`;
-    filterContainer.innerHTML = html;
     this.loadFilterBtn();
     this.resumeEventBind();
     this.loadResume();
